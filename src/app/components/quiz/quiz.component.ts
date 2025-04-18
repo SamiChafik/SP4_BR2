80c1d0b67fcef7d2e865e6ecfbf8e8e4bdd81953
SamiChafik <sami.chafik00@gmail.com>
2025-04-18 17:43:09 +0100

    added content of quiz component to display questions with a progress bar + logic to verify if the answer is correct or not + score

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { QuizService } from '../../services/quiz.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss'
 })
 export class QuizComponent {
+  selectedAnswer: string | null = null;
+  isCorrect: boolean | null = null;
 
+  get currentQuestion() {
+    return this.quizService.questions[this.quizService.currentQuestionIndex];
+  }
+
+  get isLastQuestion() {
+    return this.quizService.currentQuestionIndex === this.quizService.questions.length - 1;
+  }
+
+  constructor(public quizService: QuizService, private router: Router) {}
+
+  checkAnswer(answer: string) {
+    this.selectedAnswer = answer;
+    this.isCorrect = answer === this.currentQuestion.correct_answer;
+    
+    if (this.isCorrect) {
+      this.quizService.score++;
+    }
+  }
+
+  nextQuestion() {
+    this.selectedAnswer = null;
+    this.isCorrect = null;
+    
+    if (this.isLastQuestion) {
+      setTimeout(() => {
+        this.router.navigate(['/']);
+        this.quizService.currentQuestionIndex = 0;
+        this.quizService.score = 0;
+      }, 1000);
+    } else {
+      this.quizService.currentQuestionIndex++;
+    }
+  }
 }
