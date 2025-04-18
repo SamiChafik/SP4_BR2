dd45bb3c92e76c93b5732306bf5e75626a912c91
SamiChafik <sami.chafik00@gmail.com>
2025-04-18 17:26:10 +0100

    added routs to other components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { ResultsComponent } from './components/results/results.component';
+import { HistoryComponent } from './components/history/history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: '**', redirectTo: '' }
+];
