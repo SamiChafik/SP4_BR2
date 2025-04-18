0e075d3e4414cb572c5f5c824a34cbb793e40a32
SamiChafik <sami.chafik00@gmail.com>
2025-04-18 17:06:39 +0100

    added content to the home page (select category, defficulty, number of questions) + logic to display categories from API

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { QuizService } from '../../services/quiz.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  categories: any[] = [];
+  selectedCategory = 9;
+  selectedDifficulty = 'easy';
+  selectedNumber = 10;
 
+  constructor(private quizService: QuizService, private router: Router) {
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.quizService.getCategories().subscribe((data: any) => {
+      this.categories = data.trivia_categories;
+    });
+  }
+
+  startQuiz() {
+    this.quizService.getQuestions(this.selectedCategory, this.selectedDifficulty, this.selectedNumber)
+      .subscribe((data: any) => {
+        this.quizService.questions = data.results.map((q: any) => ({
+          ...q,
+          all_answers: this.quizService.shuffleAnswers([...q.incorrect_answers, q.correct_answer])
+        }));
+        this.router.navigate(['/quiz']);
+      });
+  }
 }
