91ea393c18d5f152e89340076620aee610b2f693
SamiChafik <sami.chafik00@gmail.com>
2025-04-18 16:51:59 +0100

    service file to Handle API calls to Open Trivia DB, manages quiz state (questions, score, progress), provides answer shuffling functionality

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
+  private apiUrl = 'https://opentdb.com';
+  questions: any[] = [];
+  currentQuestionIndex = 0;
+  score = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCategories() {
+    return this.http.get(`${this.apiUrl}/api_category.php`);
+  }
+
+  getQuestions(category: number, difficulty: string, number: number) {
+    return this.http.get(
+      `${this.apiUrl}/api.php?amount=${number}&category=${category}&difficulty=${difficulty}&type=multiple`
+    );
+  }
+
+  shuffleAnswers(answers: string[]) {
+    return [...answers].sort(() => Math.random() - 0.5);
+  }
 }
