ff9b62cadd748173f2875b2d2adf2204b453e382
SamiChafik <sami.chafik00@gmail.com>
2025-04-20 18:17:15 +0100

    history page to show all users scores + clear button to clear local storage

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
 export class HistoryComponent {
+  history: any[] = [];
 
+  constructor(private router: Router) {
+    this.loadHistory();
+  }
+
+  loadHistory() {
+    const savedHistory = localStorage.getItem('quizHistory');
+    this.history = savedHistory ? JSON.parse(savedHistory) : [];
+  }
+
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
+  clear() {
+    localStorage.clear();
+    this.history = [];
+  }
 }
